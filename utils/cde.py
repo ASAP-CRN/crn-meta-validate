3c06cac9619e010958d8c97d5eb1a7e51a2aa272
Javier Diaz-Mejia <34211920+jdime@users.noreply.github.com>
2026-05-04 17:08:17 -0400

    Add CDE v4.4 to supported versions

--- utils/cde.py
+++ utils/cde.py
@@ -354,7 +354,7 @@ def get_cde_filename(cde_version: str) -> str:
     ------
     Streamlit error and stops execution if version is unsupported
     """
-    supported_cde_versions = ["v3.4", "v4.0", "v4.1", "v4.2", "v4.3"]
+    supported_cde_versions = ["v3.4", "v4.0", "v4.1", "v4.2", "v4.3", "v4.4"]
 
     if cde_version in supported_cde_versions:
         return f"ASAP_CDE_{cde_version}"
